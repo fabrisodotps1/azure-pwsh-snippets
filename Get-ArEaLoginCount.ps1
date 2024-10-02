6b0f59b7bdbce777fe4cfa228e23913f1bcaa127
Fabrice R. <59888124+fabrisodotps1@users.noreply.github.com>
2024-10-02 22:11:49 +0200

    Added some code

--- Get-ArEaLoginCount.ps1
+++ Get-ArEaLoginCount.ps1
@@ -35,3 +35,13 @@ param(
 # KQL for Signin Logs
 # SigninLogs
 # | where TimeGenerated >= ago(180d)
+
+Connect-AzAccount
+$at = Get-AzAccessToken -ResourceUrl "https://management.azure.com"
+
+$header = @{                
+    "Authorization" = "Bearer $($at.Token)"
+    "Content-Type"  = "application/json"
+}
+
+$workspace = Invoke-RestMethod -Uri "https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{workspaceName}/tables/{tableName}?api-version=2022-10-01" -Headers $header
